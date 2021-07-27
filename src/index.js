2b0b531a0e2c47c7f61a363c209f56207dbfc947
Syransong <ksysong@gmail.com>
2021-07-26 23:47:22 -0700

    install dotenv and setup env file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+require('dotenv').config()
 
 const app = express();
+
+const mongoURI = process.env.MONGO_URI
 
 app.get("/", (req, res) => {
   res.send("Hi there!");
